db79bdfdba4545e6900ab46c1238f530d53eb26f
a0o <a0s@gihub.com>
2025-12-14 12:49:38 +0100

    fix: order of helm and crds installation

--- creator.sh
+++ creator.sh
@@ -15,5 +15,10 @@ tofu apply -auto-approve
 
 cd "${SCRIPT_DIR}/envs/dev/03_eks"
 tofu init -upgrade
-tofu apply -auto-approve -exclude="module.eks.helm_release.karpenter"
+tofu apply -auto-approve \
+  -exclude="module.eks.helm_release.karpenter" \
+  -exclude="module.eks.kubernetes_manifest.karpenter_nodepool_spot_arm64" \
+  -exclude="module.eks.kubernetes_manifest.karpenter_nodepool_spot_amd64" \
+  -exclude="module.eks.kubernetes_manifest.karpenter_ec2nodeclass_default"
+tofu apply -auto-approve -target="module.eks.helm_release.karpenter"
 tofu apply -auto-approve
